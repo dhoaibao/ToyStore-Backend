6d8b711763d2be3d8ab3edb97cd26d19b8b40e0a
Duong Hoai Bao <dhbao2505@gmail.com>
2024-12-04 03:22:56 +0700

    first setup

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import app from './src/app.js';
-import config from './src/config/index.js';
+import dotenv from 'dotenv';
 import dbConnection from './src/config/dbConnection.js';
+
+dotenv.config();
 
 async function startServer() {
     try {
         await dbConnection.migrate.latest();
         console.log('Database migrated successfully!');
 
-        const PORT = config.app.port;
+        const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,8 +1,19 @@
 import knex from "knex";
 import { Model } from "objection";
-import config from "./index.js";
 
-const knexConfig = knex(config.knex.development);
+const knexConfig = knex({
+    client: 'pg',
+    connection: {
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+        tableName: 'knex_migrations',
+    },
+});
 
 Model.knex(knexConfig);
 
